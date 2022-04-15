dd6dce2f4a3d780266b9cd4928b1bafc229307dd
mauromiguelm <masiero@imsb.biol.ethz.ch>
2022-04-15 21:32:13 +0200

    organize repository

---
+++ main/biological_analysis_of_distances.py
@@ -0,0 +1,135 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import json
+
+path_data_file = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data'
+path_fig = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\Mauro\\Cell_culture_data\\190310_LargeScreen\\figures\\pheno-ml'
+path_out = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_h1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data\\cluster_pheno-ml'
+
+os.chdir(path_out)
+
+labels = np.load('labels_nclus_bio-effect.npy')
+
+os.chdir(path_data_file)
+
+metadata_file = 'metadata-pheno-ml.json'
+
+with open(metadata_file) as output_file:
+    metadata = json.load(output_file)
+
+drugs = np.array(metadata['drug'])
+cells = np.array(metadata['cell'])
+concs = np.array(metadata['conc'])
+
+data_labels = {'labels' : labels,'drugs': drugs, 'cells':cells, "concs": concs}
+
+data_labels = pd.DataFrame(data_labels)
+
+data_labels["conf_100"] = np.nan
+data_labels["GR"] = np.nan
+
+os.chdir(path_data_file)
+
+segmentation_data = pd.read_csv("growth_curves.csv")
+
+# remove unnecessary columns
+
+segmentation_data = segmentation_data[['Time', 'Conf',  "Drug", "Final_conc_uM", "cell"]]
+
+for drug in set(drugs):
+    idx_drug = [True if x == drug else False for x in drugs ]
+
+    drug_conc = concs[idx_drug]
+    drug_cell = cells[idx_drug]
+
+    max_conc = max(drug_conc)
+
+    idx_max_conc = [True if x == max_conc else False for x in drug_conc]
+
+    drug_data = segmentation_data[segmentation_data["Drug"] == drug]
+
+    combined = [(cell, conc) for cell in set(drug_cell) for conc in set(drug_conc)]
+
+    for x in combined:
+        cell = x[0]
+        conc = float(x[1])
+        time = 100
+
+        drug_data = segmentation_data[segmentation_data["Drug"] == drug]
+        drug_data = drug_data[drug_data["cell"] == x[0]]
+        drug_data = drug_data[drug_data["Final_conc_uM"] == conc]
+        drug_data = drug_data[drug_data["Time"] == time]
+
+        data_labels.at[(data_labels['cells'] == cell) &
+                    (data_labels['drugs'] == drug) &
+                    (data_labels['concs'] == str(conc)),'conf_100'] = drug_data['Conf'].mean()
+
+os.chdir(path_fig+'\\confluence_by_cluster')
+
+for drug in set(drugs):
+
+    sub_data = data_labels[data_labels['drugs']==drug]
+
+    noEffect = sub_data[sub_data['labels']==0]['conf_100']
+    mixedEffect = sub_data[sub_data['labels']==1]['conf_100']
+    cytostatic = sub_data[sub_data['labels']==2]['conf_100']
+    cytotoxic = sub_data[sub_data['labels']==3]['conf_100']
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.boxplot([noEffect.dropna(), mixed.dropna(), cytostatic.dropna(), cytotoxic.dropna()],
+               labels=['No Effect', 'Mixed Effect', "Cytostatic", "Cytotoxic"])
+    plt.ylabel('Confluence')
+    plt.title(drug)
+    plt.xlabel('Type of effect')
+    plt.savefig(drug+'_confluence_by_cluster.png')
+
+    #todo understand why lower concentration is now shown
+
+
+
+
+### prediction of toxicity ###
+
+path_data_file = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data'
+path_fig = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\Mauro\\Cell_culture_data\\190310_LargeScreen\\figures\\pheno-ml'
+path_out = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data\\cluster_pheno-ml'
+
+os.chdir(path_out)
+
+labels = np.load('labels_nclus_bio-effect.npy')
+remote
+os.chdir(path_data_file)
+
+metadata_file = 'metadata-pheno-ml.json'
+
+with open(metadata_file) as output_file:
+    metadata = json.load(output_file)
+
+drugs = np.array(metadata['drug'])
+cells = np.array(metadata['cell'])
+concs = np.array(metadata['conc'])
+
+data_labels = {'labels' : labels,'drugs': drugs, 'cells':cells, "concs": concs}
+
+data_labels = pd.DataFrame(data_labels)
+
+# from cluster 8 take all reversible growth effects, and all irreversible effects
+# (from cytotoxic drugs) and build an predictor to differentiate between
+# control, cytostatic and cytotoxic.
+
+#select representative drugs
+
+'Methotrexate' is antimetabolite (DHFR inhibitor)
+'Docetaxel' is a microtubule inhibitor
+'Cladribine' is a purine analog that leads to DSB
+'Oxaliplatin' is a DNA crosslinking-promoting drug
+
+drugs_cytotoxic = ('Methotrexate', 'Docetaxel','Cladribine','Oxaliplatin')
+
+
+# predict results from method created before, across all dataset
+
+# plot figures
---
+++ main/parsing_distances.py
@@ -0,0 +1,200 @@
+import os, json, re
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+def get_distance_metrics(path_in, path_out):
+    """ Import distance metrics of drugs to control, and convert to array"""
+
+    dist_files = []
+
+    for path, subdirs, files in os.walk(path_in):
+        for name in files:
+            dist_files.append(os.path.join(path, name))
+
+    dist_files = [x for x in dist_files if re.search("euclidean",x)]
+
+    data = [json.load(open(x)) for x in dist_files]
+
+    drug = []
+    cell = []
+    conc = []
+
+    store_data = np.empty(shape=(0,100))
+
+    min_length = store_data.shape[1]
+
+    for idx in range(0,len(data),1):
+
+        file = dist_files[idx]
+
+        metadata = re.compile("\\\\")
+
+        metadata = metadata.split(file)
+
+        data_sub = data[idx]
+
+        time = data_sub["time"]
+
+        keep = [x > 0 for x in time]
+
+        del data_sub['time']
+
+        idx_conc = [*data_sub.keys()]
+
+        conc.append(idx_conc)
+
+        len_conc = len(idx_conc)
+
+        drug.append([re.split("_", metadata[3])[0]] * len_conc)
+
+        cell.append([re.split("_", metadata[2])[0]] * len_conc)
+
+        len_data = max(map(len, data_sub))
+
+        data_sub = np.array([x + [None] * (len_data - len(x)) for x in data_sub.values()])
+
+        time = [i for data_sub, i in enumerate(time) if keep[data_sub] == True]
+
+        data_sub = data_sub[:,keep]
+
+        if data_sub.shape[1] < min_length:
+            min_length = data_sub.shape[1]
+            store_data = store_data[:,1:min_length+1]
+        elif data_sub.shape[1] > min_length:
+            data_sub = data_sub[:,1:min_length+1]
+
+        store_data = np.append(store_data, data_sub, axis=0)
+
+        del keep
+
+    drug = [item for sublist in drug for item in sublist]
+    cell = [item for sublist in cell for item in sublist]
+    conc = [item for sublist in conc for item in sublist]
+
+    metadata = {'drug' : drug, 'cell' : cell, 'conc' : conc}
+
+    os.chdir(path_out)
+
+    with open('metadata-pheno-ml.json', 'w') as output_file:
+        json.dump(metadata, output_file)
+
+    np.save("dist_combined", store_data)
+
+def scale_results(metadata,
+                  output_file,
+                  scale,
+                  data_file = 'dist_combined.npy',
+                  path_data_file = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data',
+                  path_out = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data',
+                  apply_denoise = True):
+    "scale distance matrix by row, to avoid biass in clustering"
+
+    os.chdir(path_data_file)
+    data = np.load(data_file)
+
+    cells = metadata['cell']
+    drugs = metadata['drug']
+
+    if apply_denoise == True:
+
+        data_pd = pd.DataFrame(data.T)
+
+        data_pd = data_pd.rolling(window=5).median().T
+
+        data = data_pd.iloc[:,4:data_pd.shape[1]-1]
+
+        data = np.asarray(data)
+
+    else:
+        pass
+
+    if scale == "sample":
+
+        scaler = preprocessing.MinMaxScaler().fit(data.T)
+
+        scaled_data = scaler.transform(data.T).T
+
+        os.chdir(path_out)
+
+        np.save(output_file, scaled_data)
+
+    elif scale == "cell":
+
+        for cell in set(cells):
+
+            idx = [x == cell for x in cells]
+
+            idx = np.array(idx, dtype='bool')
+
+            data_cells = data[idx]
+
+            ncol = data_cells.shape[0]
+
+            nrow = data_cells.shape[1]
+
+            data_cells = data_cells.reshape(ncol * nrow, 1)
+
+            scaler = preprocessing.MinMaxScaler().fit(data_cells)
+
+            scaled_data = scaler.transform(data_cells)
+
+            scaled_data = scaled_data.reshape(ncol, nrow)
+
+            data[idx] = scaled_data
+
+        os.chdir(path_out)
+
+        np.save(output_file, data)
+
+    elif scale == "cell-drug":
+
+        for cell in set(cells):
+
+            idx_cells = np.array([x == cell for x in cells], dtype='bool')
+
+            for drug in set(drugs):
+
+                idx_drugs = np.array([x == drug for x in drugs],dtype="bool")
+
+                idx_cells_drugs = idx_cells & idx_drugs
+
+                data_cells = data[idx_cells_drugs]
+
+                ncol = data_cells.shape[0]
+
+                nrow = data_cells.shape[1]
+
+                data_cells = data_cells.reshape(ncol * nrow, 1)
+
+                scaler = preprocessing.MinMaxScaler().fit(data_cells)
+
+                scaled_data = scaler.transform(data_cells)
+
+                scaled_data = scaled_data.reshape(ncol, nrow)
+
+                data[idx_cells_drugs] = scaled_data
+
+            os.chdir(path_out)
+
+            np.save(output_file, data)
+
+if __name__ == '__main__':
+
+    path_data_file = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data'
+    os.chdir(path_data_file)
+    metadata_file = 'metadata-pheno-ml.json'
+
+    with open(metadata_file) as output_file:
+        metadata = json.load(output_file)
+
+    get_distance_metrics(path_in='//d.ethz.ch/groups/biol/sysbc/sauer_1/users/Mauro/from_Andrei/distances/cropped',
+                         path_out='\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data')
+
+
+    scale_results(data_file = 'dist_combined.npy',
+                  output_file = "dist_combined_celldrug-scaled_denoise",
+                  scale="cell-drug",
+                  path_data_file = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data',
+                  metadata = metadata,
+                  path_out = '\\\\d.ethz.ch\\groups\\biol\\sysbc\\sauer_1\\users\\Mauro\\Cell_culture_data\\190310_LargeScreen\\clean_data')
